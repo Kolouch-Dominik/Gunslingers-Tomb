31d88a84806ccf0acfc082e0e945efc8d8dc953b
Kolouch-Dominik <dominik.kolouch@neurosystems.cz>
2022-11-18 20:47:41 +0100

    Issue #2 effekty střel

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
@@ -8,6 +9,9 @@ public class PlayerBullet : MonoBehaviour
     public float Speed { get; set; } = 7.5f;
     [field: SerializeField]
     public Rigidbody2D BulletRB { get; set; }
+
+    [field: SerializeField] 
+    public GameObject ImpactEffect { get; set; }
     void Start()
     {
         
@@ -21,6 +25,12 @@ public class PlayerBullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Instantiate(ImpactEffect, transform.position, transform.rotation);
         Destroy(gameObject); 
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
